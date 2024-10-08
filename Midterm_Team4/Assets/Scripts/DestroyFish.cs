b9ccd7367fe83e4c7980d27b4cb705b9815b7e3c
jordanandrewww <jordan.andrew@tufts.edu>
2024-10-08 19:15:30 -0400

    different values for fish

--- Midterm_Team4/Assets/Scripts/DestroyFish.cs
+++ Midterm_Team4/Assets/Scripts/DestroyFish.cs
@@ -2,24 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class DestroyFish : MonoBehaviour
 {
     public GameHandler gameHandlerObj;
 
-    void Start(){
-          if (GameObject.FindWithTag("GameHandler") != null){
-               gameHandlerObj = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
-          }
+    void Start()
+    {
+        if (GameObject.FindWithTag("GameHandler") != null)
+        {
+            gameHandlerObj = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+        }
     }
 
     private void OnMouseDown()
     {
+        int scoreValue = GetScoreValueByTag();
+        gameHandlerObj.AddScore(scoreValue);
+        
+        // Optionally play a sound when an object is destroyed
+        // gameObject.GetComponent<AudioSource>().Play();
+        
         Destroy(gameObject);
-        //gameObject.GetComponent<AudioSource>().Play();
-        gameHandlerObj.AddScore(1);
+    }
+
+    private int GetScoreValueByTag()
+    {
+        // Using the tag of the current GameObject
+        switch (tag) 
+        {
+            case "small":
+                return 3; // Score for small fish
+            case "medium":
+                return 2; // Score for medium fish
+            case "big":
+                return 1; // Score for big fish
+            default:
+                return 0; // Default score if the tag doesn't match
+        }
     }
 }
+
 
 
 
