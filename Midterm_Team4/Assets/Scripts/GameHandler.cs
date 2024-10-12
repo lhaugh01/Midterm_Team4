2eaebd03eff199ed42ffceb1f6d76d21d5a72818
lance <lhaugh01@tufts.edu>
2024-10-12 13:56:52 -0400

    Quit and Restart game code

--- Midterm_Team4/Assets/Scripts/GameHandler.cs
+++ Midterm_Team4/Assets/Scripts/GameHandler.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour {
 
@@ -30,4 +31,17 @@ public class GameHandler : MonoBehaviour {
             Text scoreTextC = maxScoreText.GetComponent<Text>();
             scoreTextC.text = "Total Fish: " + possibleScore;
       }
+
+      public void RestartGame(){
+                playerScore = 0;
+                SceneManager.LoadScene("WilkieTesting");
+      }
+
+      public void QuitGame(){
+                #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                Application.Quit();
+                #endif
+        }
 }
